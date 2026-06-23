25e5577bfcd7ee7f9018ee5e132da9af6d47af80
uxuinur <mdnurunnabi478935@gmail.com>
2026-06-23 17:09:51 +0600

    feat : title name change

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Devin Test Project',
       theme: ThemeData(
      
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
